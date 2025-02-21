7ebebd1907b6c4b9073cbc558e6fbf1c5582f1a1
Bhargav Patel <bhargavkachhadiya@gmail.com>
2025-02-21 20:51:45 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Users = require("./models/Users");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ name: "Bhargav", surname: "Kachhadiya" });
+const url =
+  "mongodb+srv://bhargavkachhadiya1988:12345@bhargav.cmoi6kt.mongodb.net/?retryWrites=true&w=majority&appName=bhargav";
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected Successfully");
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", async (req, res) => {
+  const user = await Users.find();
+  //   res.send({ name: "Bhargav", surname: "Kachhadiya" });
+  res.send(user);
 });
 
 app.listen(4000, () => {
